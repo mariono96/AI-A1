8d9cbeaf4cad3710775c08594bd254f724975359
François Magny <francois.magny@ensimag.grenoble-inp.fr>
2017-09-18 03:56:38 +0200

    Ajout d'un guess

--- Player.java
+++ Player.java
@@ -6,6 +6,9 @@ class Player {
 		turnCount = 0;
 		notShoot = 0;
 		doShoot = 0;
+	        birdMove = new Matrix(Constants.COUNT_SPECIES, Constants.COUNT_MOVE);
+		nbBirdsSpecies = new int[Constants.COUNT_SPECIES];
+		nbRound = 0;
     }
 
     /**
@@ -116,11 +119,51 @@ class Player {
          * Here you should write your clever algorithms to guess the species of
          * each bird. This skeleton makes no guesses, better safe than sorry!
          */
-		
 
+	 double[] birdMoveCourant ;
         int[] lGuess = new int[pState.getNumBirds()];
-        for (int i = 0; i < pState.getNumBirds(); ++i)
-            lGuess[i] = Constants.SPECIES_UNKNOWN;
+        for (int i = 0; i < pState.getNumBirds(); ++i){
+            birdMoveCourant = new double[Constants.COUNT_MOVE];
+	    int nbMove = 0;
+            for (int j = 0; j < pState.getBird(i).getSeqLength(); j++){
+		if(pState.getBird(i).getObservation(j)!=-1){
+		birdMoveCourant[pState.getBird(i).getObservation(j)]++;
+		nbMove++;
+		} else {
+		    break;
+		}
+            }
+            for(int j = 0; j < Constants.COUNT_MOVE; j++){
+                birdMoveCourant[j] = birdMoveCourant[j]/nbMove;
+            }
+            int returnValue = -1 ;
+            double distance = 10;
+            for (int j = 0; j < Constants.COUNT_SPECIES; j++){
+                double sum = 0.0;
+                for (int k = 0; k < Constants.COUNT_MOVE; k++){
+                    sum += (birdMove.get(j,k)-birdMoveCourant[k])*(birdMove.get(j,k)-birdMoveCourant[k]);
+                }
+                sum = Math.sqrt(sum);
+                if (distance > sum ){
+                    distance  = sum;
+                    returnValue = j;
+                }
+            }
+             if (distance > 0.8){
+                returnValue = -1;
+            }
+            
+            switch(returnValue){
+                case 0 : lGuess[i] = Constants.SPECIES_PIGEON ; break;
+                case 1 : lGuess[i] = Constants.SPECIES_RAVEN ; break;
+                case 2 : lGuess[i] = Constants.SPECIES_SKYLARK ; break;
+                case 3 : lGuess[i] = Constants.SPECIES_SWALLOW ; break;
+                case 4 : lGuess[i] = Constants.SPECIES_SNIPE ; break;
+                case 5 : lGuess[i] = Constants.SPECIES_BLACK_STORK ; break;
+                default : lGuess[i] = Constants.SPECIES_UNKNOWN;
+            }
+        }
+        System.err.println("Salut");
         return lGuess;
     }
 
@@ -145,6 +188,57 @@ class Player {
      * @param pDue time before which we must have returned
      */
     public void reveal(GameState pState, int[] pSpecies, Deadline pDue) {
+	int[] nbBirdsSpeciesRound = new int[Constants.COUNT_SPECIES];
+        Matrix birdMoveRound = new Matrix(Constants.COUNT_SPECIES, Constants.COUNT_MOVE);
+        for (int i = 0; i < pState.getNumBirds(); i++){
+            if (pSpecies[i]!=-1){
+            for (int j = 0; j < pState.getBird(i).getSeqLength();j++){
+		if(pState.getBird(i).getObservation(j)!= -1){
+                birdMoveRound.set(pSpecies[i],pState.getBird(i).getObservation(j), birdMoveRound.get(pSpecies[i],pState.getBird(i).getObservation(j))+1);
+                nbBirdsSpeciesRound[pSpecies[i]]++;
+		}else{
+		    break;
+		}
+               
+            }
+            }
+        }
+       
+   
+        
+        for (int i = 0; i < Constants.COUNT_SPECIES;i++){
+            int nbBSC = nbBirdsSpecies[i] + nbBirdsSpeciesRound[i];
+            for (int j = 0; j < Constants.COUNT_MOVE; j++){
+                if (nbBSC!=0){
+                birdMove.set(i,j, ((birdMove.get(i,j)*nbBirdsSpecies[i]+birdMoveRound.get(i,j))/nbBSC));
+                }
+            }
+            nbBirdsSpecies[i] = nbBSC;
+        }
+        
+        
+        /*
+        for(int i  = 0; i < Constants.COUNT_SPECIES; i++){
+            for(int j = 0; j < Constants.COUNT_MOVE; j++ ){
+                if ((!( birdMoveRound.get(i,j) < 0.0001*nbBirdsSpeciesRound[i]))&&(nbBirdsSpeciesRound[i]!=0)){
+                    birdMoveRound.set(i,j, birdMoveRound.get(i,j)/nbBirdsSpeciesRound[i]);
+                } else {
+                    birdMoveRound.set(i,j, 0.0);
+                }
+            }
+        }
+        birdMove = birdMoveRound;
+        */
+        
+        System.err.print("Le nombre d'oiseau par espèce: ");
+        for (int i = 0; i < 6; i++){
+            System.err.print("  " + nbBirdsSpecies[i]);
+        }
+        System.err.println();
+        
+        //birdMove = birdMoveRound;
+        System.err.println(birdMove.toString());
+        nbRound++;
     }
 
     public static final Action cDontShoot = new Action(-1, -1);
@@ -155,4 +249,7 @@ class Player {
 	private int round;
 	private int notShoot;
 	private int doShoot;
+    private Matrix birdMove;
+    private int[] nbBirdsSpecies;
+    private int nbRound;
 }
